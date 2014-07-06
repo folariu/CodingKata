92d46ab7dd7e3c3ac4211db8bd316790bf1678c3
Florin Olariu <florin_olariu32@yahoo.com>
2014-07-06 21:02:02 +0300

    GRV1

---
+++ GildedKataV1/GildedRoseTest/GildedRoseSpecs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GildedRoseTest
+{
+    [TestClass]
+    public class GildedRoseSpecs
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
